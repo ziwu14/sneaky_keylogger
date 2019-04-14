771fe704ca051c92b7d0894b6eeac8b8218b6c0e
zihao <zw154@vcm-9397.vm.duke.edu>
2019-04-14 10:18:32 -0400

    successfully copy /etc/passwd to /tmp/passwd

--- sneaky_process.c
+++ sneaky_process.c
@@ -7,7 +7,37 @@
 int main() {
   //--------------1.print  process ID to the screen-----------------------
   printf("sneaky_process pid = %d\n", getpid());
-  //--------------2.
 
+  
+  //--------------2.copy /etc/passwd to /tmp/passwd----------------------
+  char source_file[20] = "/etc/passwd";
+  char target_file[20] = "/tmp/passwd";
+  FILE *source, *target;
+  source = fopen(source_file, "r");
+  if (source == NULL) {
+    fprintf(stderr, "fail to open file %s", source_file);
+    exit(EXIT_FAILURE);
+  }
+  target = fopen(target_file, "w");
+  if (target == NULL) {
+    fprintf(stderr, "fail to open file %s", target_file);
+    exit(EXIT_FAILURE);
+  }
+
+
+  char ch;
+  while ((ch = fgetc(source)) != EOF) {
+    fputc(ch, target);
+  }
+
+
+  fclose(source);
+  fclose(target);
+
+
+
+
+
+  
   return EXIT_SUCCESS;
 }
